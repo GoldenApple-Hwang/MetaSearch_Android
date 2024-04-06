6a13b9b28147eb708602155c08a8f2f667301cc2
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-04-06 17:19:09 +0900

    refact: MainActivity 클래스 폴더 이동

---
+++ app/src/main/java/com/example/metasearch/ui/activity/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.metasearch.ui.activity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import com.example.metasearch.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.metasearch.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int PERMISSIONS_REQUEST_READ_PHOTOS = 101;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        AppBarConfiguration appBarConfiguration =
+                new AppBarConfiguration.Builder(
+                        R.id.navigation_home,
+                        R.id.navigation_search,
+                        R.id.navigation_graph).build();
+        
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+
+        checkPermissions(); // 갤러리 사진 접근 권한 요청
+    }
+
+    // 권한 요청 메서드
+    // 필요한 권한 있으면 추가 가능
+    private void checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_MEDIA_IMAGES}, PERMISSIONS_REQUEST_READ_PHOTOS);
+        } else {
+            // Perform necessary operations if permission is already granted
+        }
+    }
+
+}
