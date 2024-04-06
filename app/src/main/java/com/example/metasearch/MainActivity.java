061ab287263eb14fe2f8443fdf2925559a81c3ca
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-04-06 13:09:57 +0900

    feat: 갤러리 사진 접근 권한 요청 기능 추가
    - 앱 실행 후 갤러리 사진에 접근을 허용하도록 요청하는 기능 추가

--- app/src/main/java/com/example/metasearch/MainActivity.java
+++ app/src/main/java/com/example/metasearch/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.metasearch;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -13,6 +17,8 @@ import androidx.navigation.ui.NavigationUI;
 import com.example.metasearch.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSIONS_REQUEST_READ_PHOTOS = 101;
 
     private ActivityMainBinding binding;
 
@@ -26,12 +32,24 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
+        AppBarConfiguration appBarConfiguration =
+                new AppBarConfiguration.Builder(
+                        R.id.navigation_home,
+                        R.id.navigation_dashboard,
+                        R.id.navigation_notifications).build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        checkPermissions(); //갤러리 접근 권한 요청
+    }
+
+    private void checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_MEDIA_IMAGES}, PERMISSIONS_REQUEST_READ_PHOTOS);
+        } else {
+            // Perform necessary operations if permission is already granted
+        }
     }
 
 }
