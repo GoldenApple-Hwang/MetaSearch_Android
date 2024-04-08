82816eb40e65d06bbea5973b1a29511d0c9f1478
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-04-08 20:36:55 +0900

    feat: 사이퍼쿼리 생성 함수 추가
    - 사용자가 자연어로 검색했을 때 relationship만 추출된 경우에 사용할 사이퍼쿼리 생성 함수 추가

--- app/src/main/java/com/example/metasearch/manager/Neo4jDatabaseManager.java
+++ app/src/main/java/com/example/metasearch/manager/Neo4jDatabaseManager.java
@@ -25,7 +25,7 @@ public class Neo4jDatabaseManager {
         return photoNames;
     }
 
-    // 사이퍼쿼리 생성 함수
+    // 사이퍼쿼리 생성 함수 : relationship과 entity를 모두 아는 경우
     public String createCypherQuery(List<String> entities, List<String> relationships, int count) {
         StringBuilder query = new StringBuilder("MATCH ");
         for (int i = 0; i < count; i++) {
@@ -35,6 +35,30 @@ public class Neo4jDatabaseManager {
             }
         }
         query.append(" RETURN photo.name AS PhotoName");
+        // 반환 형식
+        // MATCH
+        // (photo)-[:RELATIONSHIP_1]->(entity {name: "ENTITY_1"}),
+        // (photo)-[:RELATIONSHIP_2]->(entity {name: "ENTITY_2"})
+        // RETURN photo.name AS PhotoName
+        return query.toString();
+    }
+
+    // 사이퍼쿼리 생성 함수 : relationship만 아는 경우
+    public String createCypherQueryForRelationships(List<String> relationships) {
+        StringBuilder query = new StringBuilder("MATCH ");
+        for (int i = 0; i < relationships.size(); i++) {
+            query.append("(photo)-[:").append(relationships.get(i)).append("]->(entity)");
+            if (i < relationships.size() - 1) {
+                query.append(", ");
+            }
+        }
+        query.append(" RETURN photo.name AS PhotoName");
+        // 반환 형식
+        // MATCH
+        //(photo)-[:RELATIONSHIP_1]->(entity),
+        //(photo)-[:RELATIONSHIP_2]->(entity),
+        //...
+        //RETURN photo.name AS PhotoName
         return query.toString();
     }
 }
