d9b3b5f708be0e8a63035f924cc9d09c3d2d0330
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-04-13 12:31:21 +0900

    feat: 데이터 모델 추가
    - 사용자가 그린 원의 정보를 저장하기 위한 데이터 모델 Circle 클래스 추가

---
+++ app/src/main/java/com/example/metasearch/model/Circle.java
@@ -0,0 +1,13 @@
+package com.example.metasearch.model;
+
+import android.graphics.PointF;
+
+public class Circle {
+    public PointF center;
+    public float radius;
+
+    public Circle(PointF center, float radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+}
