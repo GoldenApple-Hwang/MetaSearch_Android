cca8cecfa094edd18c3f4e4fe2aca0c21c3b5af6
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-04-15 21:17:59 +0900

    refactor: 권한 요청 메서드 수정
    - 메인 액티비티에서 권한 요청하는 함수 수정
    - 버전에 따라 다른 권한 요청 수행

--- app/src/main/java/com/example/metasearch/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/metasearch/ui/activity/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.metasearch.ui.activity;
 
+import static android.content.ContentValues.TAG;
+
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.metasearch.R;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -19,6 +24,8 @@ import com.example.metasearch.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
     private static final int PERMISSIONS_REQUEST_READ_PHOTOS = 101;
     private ActivityMainBinding binding;
+
+    private boolean hasPermission = false; // 권한 받은 이후 작동되는 코드에 쓰이는 변수
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +46,41 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
-        checkPermissions(); // 갤러리 사진 접근 권한 요청
+//        checkPermissions();
+        requestStoragePermission(); // 권한 요청
+    }
+
+    // 권한 받은 이후 작동
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSIONS_REQUEST_READ_PHOTOS) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 권한이 허용된 경우, 필요한 작업 수행
+                // 예: 이미지 업로드 작업 시작
+                //이미지 분석 처리 코드
+                hasPermission = true;
+
+            } else {
+                // 권한이 거부된 경우, 사용자에게 권한이 필요한 이유를 설명하거나, 권한 없이 사용할 수 있는 기능으로 안내
+            }
+        }
+    }
+
+    // 권한 요청 메서드
+    private void requestStoragePermission() {
+        Log.d(TAG,"저장소 권한 들어감");
+        // 저장소 읽기 권한이 부여되지 않은 경우 권한 요청
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.TIRAMISU){ //Android13 이상 버전의 경우
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_MEDIA_IMAGES}, PERMISSIONS_REQUEST_READ_PHOTOS);
+            }
+
+        } // Android13 이하 버전의 경우
+        else if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE},PERMISSIONS_REQUEST_READ_PHOTOS);
+        }
+
     }
 
     // 권한 요청 메서드
