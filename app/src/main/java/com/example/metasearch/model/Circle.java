6d7a6243a80f0b0ee8f3195f357fc56713033ca7
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-04-17 18:23:29 +0900

    Update Circle.java

--- app/src/main/java/com/example/metasearch/model/Circle.java
+++ app/src/main/java/com/example/metasearch/model/Circle.java
@@ -1,17 +1,5 @@
 package com.example.metasearch.model;
 
-import android.graphics.PointF;
-
-//public class Circle {
-//    public PointF center;
-//    public float radius;
-//
-//    public Circle(PointF center, float radius) {
-//        this.center = center;
-//        this.radius = radius;
-//    }
-//}
-
 public class Circle {
     private float centerX;
     private float centerY;
