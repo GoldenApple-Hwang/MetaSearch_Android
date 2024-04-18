8fbf50597233010d568b1e45292d44c4aa9b11a2
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-04-18 16:11:15 +0900

    Create ApiService.java

---
+++ app/src/main/java/com/example/metasearch/service/ApiService.java
@@ -0,0 +1,24 @@
+package com.example.metasearch.service;
+
+import com.example.metasearch.model.CircleDetectionResponse;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.http.Multipart;
+import retrofit2.http.Part;
+
+public interface ApiService {
+    @Multipart
+    @POST("upload")
+    Call<CircleDetectionResponse> uploadImageAndCircles(
+            @Part MultipartBody.Part image,
+            @Part("source")RequestBody source,
+            @Part("circles") RequestBody circles
+    );
+    @POST("/android/circleToSearch/youjeong")
+    Call<ResponseBody> sendDetectedObjects(@Body RequestBody detectedObjects);
+}
