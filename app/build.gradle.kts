f30da1be376a0cada8d641ce6c6eb7e0ae509648
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-04-19 16:11:34 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,8 @@ android {
 
     defaultConfig {
         applicationId = "com.example.metasearch"
-        minSdk = 33
+//        minSdk = 33
+        minSdk = 31
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
