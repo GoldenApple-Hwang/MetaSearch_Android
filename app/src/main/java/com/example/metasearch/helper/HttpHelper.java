fc04df27be348dfbb1144a349603ee41c76e1b39
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-04-25 14:42:53 +0900

    feat: HttpHelper에 멀티 인스턴스 지원 추가
    - HttpHelper 클래스를 통해 여러 베이스 URL에 대한 인스턴스 관리 기능 구현

--- app/src/main/java/com/example/metasearch/helper/HttpHelper.java
+++ app/src/main/java/com/example/metasearch/helper/HttpHelper.java
@@ -2,6 +2,8 @@ package com.example.metasearch.helper;
 
 import com.example.metasearch.service.ApiService;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
@@ -9,38 +11,35 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class HttpHelper {
-    private OkHttpClient okHttpClient;
+    private static Map<String, HttpHelper> instances = new HashMap<>();
     private Retrofit retrofit;
-    private static HttpHelper httpHelperObject;
 
-    private HttpHelper(String BASE_URL){
-        okHttpClient = new OkHttpClient.Builder() //응답을 1분으로 지정
+    private HttpHelper(String baseUrl) {
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
                 .connectTimeout(60, TimeUnit.SECONDS)
                 .readTimeout(60, TimeUnit.SECONDS)
                 .writeTimeout(60, TimeUnit.SECONDS)
                 .build();
 
         retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
+                .baseUrl(baseUrl)
                 .addConverterFactory(GsonConverterFactory.create())
-                .client(okHttpClient) // 위에서 설정한 OkHttpClient 인스턴스 사용
+                .client(okHttpClient)
                 .build();
-
     }
-    //retrofit 반환
-    public Retrofit getRetrofit(){
+
+    public static synchronized HttpHelper getInstance(String baseUrl) {
+        if (!instances.containsKey(baseUrl)) {
+            instances.put(baseUrl, new HttpHelper(baseUrl));
+        }
+        return instances.get(baseUrl);
+    }
+
+    public Retrofit getRetrofit() {
         return retrofit;
     }
 
-    //HttpHelper 객체 반환
-    public static HttpHelper getInstance(String BASE_URL){ //객체 생성(싱글톤 구현)
-        if(httpHelperObject == null){
-            httpHelperObject = new HttpHelper(BASE_URL);
-        }
-        return httpHelperObject;
-    }
-
     public ApiService create(Class<ApiService> apiServiceClass) {
-        return retrofit.create(ApiService.class);
+        return retrofit.create(apiServiceClass);
     }
 }
