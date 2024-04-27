f7c9c9e4aa15d584981db8ecf4f319320eab293b
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-04-27 16:15:31 +0900

    feat: NL 쿼리 요청 모델 추가
    - NLQueryRequest 클래스를 추가하여 데이터베이스 이름과 쿼리 문자열을 관리

---
+++ app/src/main/java/com/example/metasearch/model/NLQueryRequest.java
@@ -0,0 +1,28 @@
+package com.example.metasearch.model;
+
+public class NLQueryRequest {
+    private String dbName;
+    private String query;
+
+    public NLQueryRequest(String dbName, String query) {
+        this.dbName = dbName;
+        this.query = query;
+    }
+
+    // Getters and Setters
+    public String getDbName() {
+        return dbName;
+    }
+
+    public void setDbName(String dbName) {
+        this.dbName = dbName;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+}
