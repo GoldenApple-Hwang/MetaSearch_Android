e29ee2ec3d2a6712b3a4176a78ae22ac7980c566
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-05-11 21:14:12 +0900

    feat: 검색된 사진 중복 출력 수정

--- app/src/main/java/com/example/metasearch/ui/activity/CircleToSearchActivity.java
+++ app/src/main/java/com/example/metasearch/ui/activity/CircleToSearchActivity.java
@@ -29,7 +29,9 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import io.github.muddz.styleabletoast.StyleableToast;
 import me.jfenn.colorpickerdialog.dialogs.ColorPickerDialog;
@@ -58,18 +60,16 @@ public class CircleToSearchActivity extends AppCompatActivity
         setContentView(binding.getRoot());
 
         imageUri = Uri.parse(getIntent().getStringExtra("imageUri"));
-//        binding.customImageView.setImageUri(imageUri);
+        // Glide를 사용하여 이미지 로드 및 표시(이미지 자동 회전 방지)
+        Glide.with(this)
+                .load(imageUri)
+                .into(binding.customImageView);
 
         // 화면 크기의 1/2 높이로 CustomImageView 크기 조정
         int halfScreenHeight = getResources().getDisplayMetrics().heightPixels / 2;
         ViewGroup.LayoutParams layoutParams = binding.customImageView.getLayoutParams();
         layoutParams.height = halfScreenHeight;
         binding.customImageView.setLayoutParams(layoutParams);
-
-        // Glide를 사용하여 이미지 로드 및 표시(이미지 자동 회전 방지)
-        Glide.with(this)
-                .load(imageUri)
-                .into(binding.customImageView);
 
         // circle to search 결과 검색된 사진 출력하는 세로 방향 RecyclerView 세팅
         setupRecyclerView();
@@ -120,10 +120,8 @@ public class CircleToSearchActivity extends AppCompatActivity
             public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
                 super.onScrolled(recyclerView, dx, dy);
                 if (dy > 0) {
-                    // 스크롤 내릴 때, 네비게이션 바 숨기기
                     hideBottomNavigationView();
                 } else if (dy < 0) {
-                    // 스크롤 올릴 때, 네비게이션 바 보이기
                     showBottomNavigationView();
                 }
             }
@@ -135,23 +133,20 @@ public class CircleToSearchActivity extends AppCompatActivity
     }
     // 하단의 네비 바 보임
     public void showBottomNavigationView() {
-        binding.customImageView.animate().translationY(0);
+        binding.circleMenu.animate().translationY(0);
     }
-
     private void sendCirclesAndImage() throws IOException {
         List<Circle> circles = binding.customImageView.getCircles();
         if (imageUri != null && !circles.isEmpty()) {
-//            binding.circleMenu.setEnabled(false); // 버튼 비활성화
-            // 메뉴 아이템을 직접 비활성화
+            // 검색 버튼 비활성화
             MenuItem searchItem = binding.circleMenu.getMenu().findItem(R.id.search);
             searchItem.setEnabled(false);
-            // 요청 전에 로딩 애니메이션 표시
+            // 요청 전에 로딩 아이콘 표시
             binding.spinKit.setVisibility(View.VISIBLE);
 
             // AI Server로 이미지와 원 리스트 전송
             aiRequestManager.uploadCircleData(imageUri, circles, "source", this, this);
         } else {
-//            Toast.makeText(this, "이미지 또는 원 정보가 없습니다.", Toast.LENGTH_SHORT).show();
             StyleableToast.makeText(this, "이미지 또는 원 정보가 없습니다. 드래그 해서 원을 그려주세요.", R.style.customToast).show();
         }
     }
@@ -159,21 +154,19 @@ public class CircleToSearchActivity extends AppCompatActivity
         ImageAdapter adapter = new ImageAdapter(imageUris, this, this);
         binding.circleToSearchRecyclerView.setAdapter(adapter);
     }
-
     private void updateRecyclerViewWithResponse(PhotoResponse photoResponse) {
+        Set<String> uniquePhotoNames = new HashSet<>(); // 중복을 허용하지 않는 Set 컬렉션 생성
         List<Uri> matchedUris = new ArrayList<>();
         if (photoResponse != null && photoResponse.getPhotos() != null) {
-            List<String> allPhotoNames = new ArrayList<>();
+            // Common photos 추가
+            uniquePhotoNames.addAll(photoResponse.getPhotos().getCommonPhotos());
 
-            // Common photos 처리
-            allPhotoNames.addAll(photoResponse.getPhotos().getCommonPhotos());
-
-            // Individual photos 처리
+            // Individual photos 추가
             for (List<String> names : photoResponse.getPhotos().getIndividualPhotos().values()) {
-                allPhotoNames.addAll(names);
+                uniquePhotoNames.addAll(names);
             }
             // 이미지 이름 리스트에서 확장자를 제거하지 않고 사용
-            matchedUris = GalleryImageManager.findMatchedUris(allPhotoNames, this);
+            matchedUris = GalleryImageManager.findMatchedUris(new ArrayList<>(uniquePhotoNames), this);
         }
 
         ImageAdapter adapter = new ImageAdapter(matchedUris, this, this);
@@ -198,9 +191,11 @@ public class CircleToSearchActivity extends AppCompatActivity
         // 카테고리 이름만 TextView에 표시
         StringBuilder categories = new StringBuilder();
         photoResponse.getPhotos().getIndividualPhotos().keySet().forEach(category -> {
-            categories.append(category).append("\n");
+//            categories.append(category).append("\n");
+            categories.append("#").append(category).append("  ");
         });
-        binding.textViewRelatedCategories.setText(categories.toString().trim());
+//        binding.textViewRelatedCategories.setText(categories.toString().trim());
+        binding.textViewRelatedCategories.setText(categories.toString());
     }
     @Override
     public void onImageClick(Uri uri) {
@@ -211,15 +206,13 @@ public class CircleToSearchActivity extends AppCompatActivity
     @Override
     public void onCircleUploadSuccess(List<String> detectedObjects) {
         runOnUiThread(() -> {
-//            binding.circleMenu.setEnabled(true);
             MenuItem searchItem = binding.circleMenu.getMenu().findItem(R.id.search);
-            searchItem.setEnabled(true);
-            binding.spinKit.setVisibility(View.GONE);
+            searchItem.setEnabled(true); // 검색 버튼 활성화
+            binding.spinKit.setVisibility(View.GONE); // 로딩 아이콘 숨김
             if (detectedObjects.isEmpty()) {
                 StyleableToast.makeText(this, "No objects detected.", R.style.customToast).show();
             } else {
                 // Web Server로 이미지 분석 결과 전송
-//                webRequestManager.sendDetectedObjectsToAnotherServer(detectedObjects, "youjeong", this);
                 webRequestManager.sendDetectedObjectsToAnotherServer(detectedObjects, DatabaseUtils.getPersistentDeviceDatabaseName(this), this);
             }
         });
@@ -227,25 +220,23 @@ public class CircleToSearchActivity extends AppCompatActivity
     @Override
     public void onCircleUploadFailure(String message) {
         runOnUiThread(() -> {
-//            binding.circleMenu.setEnabled(true);
             MenuItem searchItem = binding.circleMenu.getMenu().findItem(R.id.search);
-            searchItem.setEnabled(true);
-            binding.spinKit.setVisibility(View.GONE);
+            searchItem.setEnabled(true); // 검색 버튼 비활성화
+            binding.spinKit.setVisibility(View.GONE); // 로딩 아이콘 숨김
             StyleableToast.makeText(this, "Upload failed: " + message, R.style.customToast).show();
         });
     }
     @Override
     public void onWebServerUploadSuccess(PhotoResponse photoResponse) {
         MenuItem searchItem = binding.circleMenu.getMenu().findItem(R.id.search);
-        searchItem.setEnabled(true);
-//        binding.circleMenu.setEnabled(true); // 버튼 활성화
-        binding.spinKit.setVisibility(View.GONE); // 로딩 애니메이션 숨김
-        updateRecyclerViewWithResponse(photoResponse);
+        searchItem.setEnabled(true); // 검색 버튼 활성화
+        binding.spinKit.setVisibility(View.GONE); // 로딩 아이콘 숨김
+        updateRecyclerViewWithResponse(photoResponse); // 검색된 사진으로 화면 업데이트
     }
     @Override
     public void onWebServerUploadFailure(String message) {
         MenuItem searchItem = binding.circleMenu.getMenu().findItem(R.id.search);
-        searchItem.setEnabled(true);
+        searchItem.setEnabled(true); // 검색 버튼 활성화
     }
 }
 
