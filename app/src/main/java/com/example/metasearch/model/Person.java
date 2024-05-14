8360e18edf63c81bbc0260851759e5cc6879dab7
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-05-14 18:33:49 +0900

    feat: Person 데이터 모델 수정
    - 인물 이름 삭제, id 값 추가

--- app/src/main/java/com/example/metasearch/model/Person.java
+++ app/src/main/java/com/example/metasearch/model/Person.java
@@ -1,23 +1,23 @@
 package com.example.metasearch.model;
 
 public class Person {
-    private String imageName; // 사진 이름
-    private String userName; // 사용자가 입력한 이름
+
+    private int id; // 고유 ID
+    private String imageName; // 사진 이름(인물 이름과 동일)
     private byte[] image; // 홈 화면 상단에 작게 표시되는 얼굴 이미지를 위한 데이터
     private String phone; // 인물의 전화번호. 기본값은 "".
     private Integer isDelete; // 삭제된 인물인지 나타냄. 기본값은 1, 삭제되면 0
 
-    public Person(String imageName, byte[] image) {
+    public Person(int id, String imageName, byte[] image) {
+        this.id = id;
         this.imageName = imageName;
-        this.userName = userName != null ? userName : "";
         this.image = image;
         this.phone = "";
         this.isDelete = 0;
     }
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
     public String getImageName() { return imageName; }
-    public String getUserName() {
-        return userName;
-    }
     public byte[] getImage() {
         return image;
     }
@@ -25,9 +25,6 @@ public class Person {
     public Integer getIsDelete() { return isDelete; }
     public void setImageName(String imageName) {
         this.imageName = imageName;
-    }
-    public void setUserName(String userName) {
-        this.userName = userName;
     }
     public void setImage(byte[] image) {
         this.image = image;
@@ -39,23 +36,3 @@ public class Person {
         this.isDelete = isDelete;
     }
 }
-
-
-//public class Person {
-//    private String imageName; // 사진 이름
-//    private String userName; // 사용자가 입력한 이름
-//    private byte[] image; // 홈 화면 상단에 작게 표시되는 얼굴 이미지를 위한 데이터
-//
-//    public Person(String imageName, String name, byte[] image) {
-//        this.imageName = imageName;
-//        this.userName = name;
-//        this.image = image;
-//    }
-//    public String getImageName() { return imageName; }
-//    public String getName() {
-//        return userName;
-//    }
-//    public byte[] getImage() {
-//        return image;
-//    }
-//}
