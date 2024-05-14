090a2a2472e172b8a6eb7b894e8f38c410bcca06
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-05-14 19:36:14 +0900

    feat: 인물 사진 롱클릭 시 인물 리스트에서 제거하는 기능 추가
    - 다이얼로그를 띄워 인물 제거 여부를 물어봄

--- app/src/main/java/com/example/metasearch/ui/adapter/PersonAdapter.java
+++ app/src/main/java/com/example/metasearch/ui/adapter/PersonAdapter.java
@@ -11,17 +11,21 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.metasearch.R;
+import com.example.metasearch.dao.DatabaseHelper;
 import com.example.metasearch.model.Person;
 import com.example.metasearch.ui.activity.PersonPhotosActivity;
+import com.example.metasearch.ui.fragment.HomeFragment;
 
 import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class PersonAdapter extends RecyclerView.Adapter<PersonAdapter.PersonViewHolder> {
+    private DatabaseHelper databaseHelper;
     private final List<Person> people;
     private ImageAdapter.OnImageClickListener listener;
     private final Context context;
@@ -30,6 +34,7 @@ public class PersonAdapter extends RecyclerView.Adapter<PersonAdapter.PersonView
         this.people = people;
         this.listener = listener;
         this.context = context;
+        databaseHelper = DatabaseHelper.getInstance(context);
     }
 
     public interface OnImageClickListener {
@@ -46,7 +51,22 @@ public class PersonAdapter extends RecyclerView.Adapter<PersonAdapter.PersonView
             nameView = view.findViewById(R.id.name);
         }
     }
-
+    private void showDeletePersonDialog(int personId) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context, R.style.CustomAlertDialogTheme);
+        builder.setTitle("인물 리스트 수정");
+        builder.setMessage("이 인물을 '내가 아는 사람들'에서 삭제하시겠습니까?");
+        builder.setPositiveButton("삭제", (dialog, which) -> {
+            // 데이터 삭제 로직 실행
+            deletePerson(personId);
+        });
+        builder.setNegativeButton("취소", (dialog, which) -> dialog.dismiss());
+        builder.show();
+    }
+    // 실제 삭제는 아니고, 화면에 표시 여부만 변경
+    private void deletePerson(int personId) {
+        databaseHelper.markPersonAsDeleted(personId);
+        // 화면 업데이트 필요
+    }
     @NonNull
     @Override
     public PersonViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -69,11 +89,14 @@ public class PersonAdapter extends RecyclerView.Adapter<PersonAdapter.PersonView
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(context, PersonPhotosActivity.class);
-                intent.putExtra("personName", person.getUserName()); // 인물 이름 전달
-                intent.putExtra("imageName", person.getImageName()); // 사진 이름 전달
-                intent.putExtra("imageData", person.getImage()); // 사진(바이트 배열) 전달
+                intent.putExtra("id", person.getId());
                 context.startActivity(intent); // 클릭한 인물이 나온 사진을 모두 찾아서 보여주는 화면으로 전환
             }
+        });
+
+        holder.imageView.setOnLongClickListener(v -> {
+            showDeletePersonDialog(person.getId());
+            return true; // 롱 클릭 이벤트 처리 완료
         });
     }
     @Override
