462379e6264a8192e003f5371766847eaee7bbf9
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-05-18 15:16:33 +0900

    feat: 좋아요 화면 삭제
    - 홈 메뉴 바에서 좋아요 아이템 삭제

--- app/src/main/java/com/example/metasearch/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/metasearch/ui/activity/MainActivity.java
@@ -54,7 +54,8 @@ public class MainActivity extends AppCompatActivity {
     private void setupNavigation() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_search, R.id.navigation_graph, R.id.navigation_like
+                R.id.navigation_home, R.id.navigation_search, R.id.navigation_graph
+//                , R.id.navigation_like
         ).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
